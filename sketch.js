57348e933630bfcac78d577a2168bcb83732f2b0
swathiuppala <79951582+swathiuppala@users.noreply.github.com>
2022-01-21 19:35:30 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var box;
+function setup() {
+  createCanvas(400,400);
+  box = createSprite(200,200,70,70);
+  box.shapeColor = "red";
+}
+
+function draw() 
+{
+  background(30);
+
+  drawSprites();
+
+}
+
+
+
+
